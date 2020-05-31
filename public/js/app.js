5a05574f05423a8405007c1574a0e51b7bcfff5c
johir-rayhan <johir.rayhan01@gmail.com>
2020-05-31 22:19:14 +0600

    Some basics about the vue instance

--- public/js/app.js
+++ public/js/app.js
@@ -1,86 +1,13 @@
 new Vue({
  el: '#app',
  data: {
-   playerHealth: 100,
-   monsterHealth: 100,
-   gameIsRunning: false,
-   turns: []
+   title: 'The VueJS instance',
+   showPharagraph: false
  },
  methods: {
-   startGame: function() {
-     this.gameIsRunning = true;
-     this.playerHealth = 100;
-     this.monsterHealth = 100;
-     this.turns = [];
-   },
-   attack: function() {
-    let damage = this.calculateDamage(10,3);
-    this.monsterHealth -= damage;
-    this.turns.unshift({
-      isPlayer: true,
-      text: 'Player hits monstar for ' + damage
-    });
-    if( this.checkWin()){
-      return;
-    }
-    this.monsterAttack();
-   },
-   specialAttack: function() {
-    let damage = this.calculateDamage(20,10);
-    this.monsterHealth -= damage;
-    this.turns.unshift({
-      isPlayer: true,
-      text: 'Player hits monstar hard for ' + damage
-    });
-    if( this.checkWin()){
-      return;
-    }
-    this.monsterAttack();
-   },
-   heal: function() {
-     if (this.playerHealth <=90) {
-       this.playerHealth += 10
-     } else {
-       this.playerHealth = 100
-     }
-     this.turns.unshift({
-      isPlayer: true,
-      text: 'Player heal for 10'
-    });
-    this.monsterAttack();
-   },
-   giveUp: function() {
-    this.gameIsRunning = false;
-   },
-   monsterAttack: function() {
-    let damage = this.calculateDamage(12,5);
-    this.playerHealth -= damage;
-    this.turns.unshift({
-      isPlayer: false,
-      text: 'Monster hits player for ' + damage
-    });
-    this.checkWin();
-   },
-   calculateDamage: function(max, min) {
-     return Math.max(Math.floor(Math.random() * max) + 1, min);
-   },
-   checkWin: function() {
-     if ( this.monsterHealth <=0 ) {
-       if (confirm("You won! New game?")) {
-         this.startGame();
-       } else {
-         this.gameIsRunning = false;
-       }
-       return true;
-     } else if ( this.playerHealth <=0 ) {
-      if (confirm("You lost! New game?")) {
-        this.startGame();
-      } else {
-        this.gameIsRunning = false;
-      }
-      return true;
-     }
-     return false;
+   show: function() {
+     this.showPharagraph = true;
+     this.title = 'The VueJS instance (Update)'
    }
  }
 });
