7b6aa7419cebc2e7f1fda7eddcf60e2d4aded975
johir-rayhan <johir.rayhan01@gmail.com>
2020-06-01 22:34:08 +0600

    Storing data in components with the data method

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
+// const data = { status: "Critial" }; // it's not the right way to use 
+
 Vue.component('my-tag', {
-  data: function() {
-    return {status: "Critial"}
+  data: function () {
+    return { status: 'Critical'};
   },
-  template: "<h2>Server Status: {{ status }}</h2>"
+  methods: {
+    changeStatus: function () {
+      this.status = "Normal"
+    }
+  },
+  template: "<h2>Server Status: {{ status }} (<button @click='changeStatus'>Change</button>)</h2>"
 })
 
 new Vue({
