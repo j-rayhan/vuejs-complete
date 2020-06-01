dd7df60c74ea6661d5721add096beff2b6459074
johir-rayhan <johir.rayhan01@gmail.com>
2020-06-01 23:52:29 +0600

    Mobing to a beatter Folder structure

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,18 @@
 </template>
 
 <script>
+import Header from './components/Shared/Header.vue';
+import Footer from './components/Shared/Footer.vue';
+import Servers from './components/Server/ServerStatus.vue';
+import ServerDetails from './components/Server/ServerDetails.vue';
+export default {
+  components: {
+    'app-header': Header,
+    'app-servers': Servers,
+    'app-server-details': ServerDetails,
+    'app-footer': Footer
+  }
+}
 </script>
 
 <style>
---
+++ src/components/Server/ServerStatus.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Server status: {{ status }}</h1>
+    <button @click="changeStatus">Change</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({status: 'Critical'}),
+  methods: {
+    changeStatus() {
+      this.status = 'Normal';
+    }
+  }
+}
+</script>
