1adbe8ee6ab0fc89eda2fc811a5d55fd187f5c25
johir-rayhan <johir.rayhan01@gmail.com>
2020-06-02 22:40:29 +0600

    Naming props

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,13 +2,13 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
-        <p>User Name: {{ name }} </p>
+        <p>User Name: {{ myName }} </p>
     </div>
 </template>
 
 <script>
 export default {
-    props: [ 'name' ]
+    props: [ 'myName' ]
 }
 </script>
 
