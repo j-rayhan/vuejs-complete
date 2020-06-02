048e3f0bbe85a2c6772d7cbec0a2e8faf95af804
johir-rayhan <johir.rayhan01@gmail.com>
2020-06-02 07:50:05 +0600

    Introduction

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <template>
   <div class="container">
-    <app-header></app-header>
-    <hr>
-    <div class="row">
-      <app-servers></app-servers>
-      <app-server-details></app-server-details>
-    </div>
-    <hr>
-    <app-footer></app-footer>
+      <div class="row">
+          <div class="col-xs-12">
+              <app-user></app-user>
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
-import Header from './components/Shared/Header.vue';
-import Footer from './components/Shared/Footer.vue';
-import Servers from './components/Server/ServerStatus.vue';
-import ServerDetails from './components/Server/ServerDetails.vue';
+import User from './components/User';
+
 export default {
   components: {
-    appHeader: Header,
-    'app-servers': Servers,
-    'app-server-details': ServerDetails,
-    'app-footer': Footer
+    appUser: User,
   }
 }
 </script>
 
 <style>
+  div.component {
+      border: 1px solid black;
+      padding: 30px;
+  }
 </style>
