6845851fc34edde3e8141a182e55cefe6b29b8de
johir-rayhan <johir.rayhan01@gmail.com>
2020-06-07 08:18:26 +0600

    Setting up module project

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,22 @@
   <div class="container">
       <div class="row">
           <div class="col-xs-12">
-              <app-user></app-user>
+              <app-quote></app-quote>
           </div>
       </div>
   </div>
 </template>
 
 <script>
-import User from './components/User';
+import Quote from './components/Quote';
 
 export default {
   components: {
-    appUser: User,
+    appQuote: Quote,
   }
 }
 </script>
 
 <style>
-  div.component {
-      border: 1px solid black;
-      padding: 30px;
-  }
+ 
 </style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,19 @@
+<template>
+ <div>
+  <p>A Wonderful Quote!</p>
+ </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+ div {
+  border: 1px solid #ccc;
+  box-shadow: 1px 1px 2px black;
+  padding: 30px;
+  margin: 30px auto;
+  text-align: center;
+ }
+</style>
