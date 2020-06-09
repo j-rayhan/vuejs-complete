5ebdde5f9fb32ecf3edb7ebb93cc1de4e91998d3
johir-rayhan <johir.rayhan01@gmail.com>
2020-06-09 09:06:26 +0600

    Switching multiple components

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
   <div class="container">
       <div class="row">
           <div class="col-xs-12">
+            <button @click="selectedComponent = 'appQuote'">Quote</button>
+            <button @click="selectedComponent = 'appAuthor'">Author</button>
+            <button @click="selectedComponent = 'appNewQuote'">New Quote</button>
+            <hr>
+            <p> {{ selectedComponent }} </p>
               <app-quote>
                 <h2 slot="title"> {{ quoteTitle }} </h2>
                 <p>A Wonderful Quote!</p>
@@ -13,15 +18,20 @@
 
 <script>
 import Quote from './components/Quote';
+import Author from './components/Author';
+import NewQuote from './components/NewQuote';
 
 export default {
   data: function () {
     return {
-      quoteTitle: "The Quote!"
+      quoteTitle: "The Quote!",
+      selectedComponent: 'appQuote',
     }
   },
   components: {
     appQuote: Quote,
+    appAuthor: Author,
+    appNewQuote: NewQuote,
   }
 }
 </script>
---
+++ src/components/NewQuote.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>New quote</h2>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
