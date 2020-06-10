cc024a7fd80626b8a249f7ec71195aa3294f8db9
johir-rayhan <johir.rayhan01@gmail.com>
2020-06-10 23:28:29 +0600

    Setup project 2

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,17 @@
 <template>
   <div class="container">
-      <div class="row">
-          <div class="col-xs-12">
-            <button @click="selectedComponent = 'appQuote'">Quote</button>
-            <button @click="selectedComponent = 'appAuthor'">Author</button>
-            <button @click="selectedComponent = 'appNewQuote'">New Quote</button>
-            <hr>
-            <p> {{ selectedComponent }} </p>
-            <keep-alive>
-              <component :is="selectedComponent">Default Text</component>
-            </keep-alive>
-              <!-- <app-quote>
-                <h2 slot="title"> {{ quoteTitle }} </h2>
-                <p>A Wonderful Quote!</p>
-              </app-quote> -->
-          </div>
-      </div>
+      {{ maxQuotes }}
   </div>
 </template>
 
 <script>
-import Quote from './components/Quote';
-import Author from './components/Author';
-import NewQuote from './components/NewQuote';
 
 export default {
   data: function () {
     return {
-      quoteTitle: "The Quote!",
-      selectedComponent: 'appQuote',
+      maxQuotes: 10,
     }
   },
-  components: {
-    appQuote: Quote,
-    appAuthor: Author,
-    appNewQuote: NewQuote,
-  }
 }
 </script>
 
