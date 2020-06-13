55423e0bdebb7125352ee6f9b4ff96a29214df34
johir-rayhan <johir.rayhan01@gmail.com>
2020-06-14 03:56:12 +0600

    Allowing users to create quotes

---
+++ src/components/NewQuote.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="row">
+    <form>
+     <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+      <label for="quote">Quote</label>
+      <textarea 
+      name="quote" 
+      id="quote" 
+      cols="30" 
+      rows="3" 
+      v-model="quote" 
+      class="form-control">
+      </textarea>
+     </div>
+     <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+      <button class="btn btn-primary" @click.prevent="createNew">Add New</button>
+     </div>
+    </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+ data: () => ({
+  quote: ''
+ }),
+ methods: {
+  createNew() {
+   
+  }
+ }
+}
+</script>
+
+<style>
+
+</style>
