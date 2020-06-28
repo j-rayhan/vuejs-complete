f58d5896829a03165c9f1ff872602f494306fd8a
johir-rayhan <johir.rayhan01@gmail.com>
2020-06-29 03:58:35 +0600

    Creating a custom directive

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+Vue.directive('highlight', {
+  bind(el, binding, vnode) {
+    el.style.backgroundColor = 'green'
+  }
+})
+
 export const eventBus = new Vue({
   methods: {
     changeAge(age){
