2bd4fc63a4c659ab1bb245796d3f52caadb1652b
johir-rayhan <johir.rayhan01@gmail.com>
2020-07-01 02:34:08 +0600

    Creating a global mixin

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.filter('to-LowerCase', (value) => value.toLowerCase())
+Vue.mixin({
+  created(){
+    console.log('PRINT IN %s=====>','Global', 'mixin created');
+  }
+})
 
 new Vue({
   el: '#app',
