a2bd121a569c423d6fa922fefb66d7b28a94270b
johir-rayhan <johir.rayhan01@gmail.com>
2020-07-01 01:11:57 +0600

    creating a local filter

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,22 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Built-in Directives</h1>
-        <p v-text="'Some text'"></p>
-        <p v-html="'<strong>Some strong text</strong>'"></p>
-      </div>
-    </div>
-    <hr>
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Custom Directives</h1>
-        <p v-highlight:background.delayed="'yellow'">Color this</p>
-        <p v-highlight:background="'red'">Color this</p>
-        <p v-highlight="'green'">Color this</p>
-        <hr>
-        
-        <p v-local-highlight:background.delayed="'green'">Local Color this</p>
-        <p v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor: 'blue', delay: 500 }">Local Color this</p>
-      </div>
+      <h1>Filters && Mixins</h1>
+      <p>{{ text | toUpperCase }}</p>
     </div>
   </div>
 </template>
 
 <script>
-import Switch from './components/Switch'
 export default {
-  directives: {
-    'local-highlight': {
-      bind(el, binding, vnode) {
-      let delay = 0;
-      if (binding.modifiers['delayed']) delay = 3000;
-      if (binding.modifiers['blink']) {
-        let mainColor = binding.value.mainColor,
-            secondColor = binding.value.secondColor,
-            currentColor = mainColor;
-            setTimeout(() => {
-              setInterval(() => {
-                currentColor === secondColor ? currentColor = mainColor : currentColor = secondColor;
-                if (binding.arg === 'background') el.style.backgroundColor = currentColor;
-                else el.style.color = currentColor;
-              }, binding.value.delay);
-            }, delay);
-      } else {
-        setTimeout(() => {
-          if (binding.arg === 'background') {
-            el.style.backgroundColor = binding.value
-          } else {
-            el.style.color = binding.value 
-            el.style.fontSize = '36px'
-          }
-        }, delay);
-      }
-      }
+  data() {
+    return {
+      text: "Hello there!"
+    }
+  },
+  filters: {
+    toUpperCase(value){
+      return value.toUpperCase();
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,29 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.directive('highlight', {
-  bind(el, binding, vnode) {
-    // el.style.backgroundColor = 'green'
-    let delay = 0;
-    if (binding.modifiers['delayed']) delay = 3000;
-    setTimeout(() => {
-      if (binding.arg === 'background') {
-        el.style.backgroundColor = binding.value
-      } else {
-        el.style.color = binding.value 
-        el.style.fontSize = '26px'
-      }
-    }, delay);
-  }
-})
-
-export const eventBus = new Vue({
-  methods: {
-    changeAge(age){
-      this.$emit('ageWasEdit', age)
-    }
-  }
-});
 new Vue({
   el: '#app',
   render: h => h(App)
