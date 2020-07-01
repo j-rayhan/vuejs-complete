37d47138cb154146b340403a0c3a4b73d117f532
johir-rayhan <johir.rayhan01@gmail.com>
2020-07-02 04:15:15 +0600

    Error in route(component was not reload/updated

--- src/Header.vue
+++ src/Header.vue
@@ -1,7 +1,8 @@
 <template>
   <ul class="nav nav-pills">
    <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
-   <router-link to="/user/5" tag="li" active-class="active" exact><a>User</a></router-link>
+   <router-link to="/user/1" tag="li" active-class="active" exact><a>User 1</a></router-link>
+   <router-link to="/user/2" tag="li" active-class="active" exact><a>User 2</a></router-link>
   </ul>
 </template>
 
