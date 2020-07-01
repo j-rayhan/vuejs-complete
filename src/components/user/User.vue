bde5d4a74ec35907dcef41d09a6af0aa8bff92b9
johir-rayhan <johir.rayhan01@gmail.com>
2020-07-02 04:18:47 +0600

    Fix error

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -15,6 +15,11 @@ export default {
       id
     }
   },
+  watch: {
+    '$route'(to, from){
+      this.id = to.params.id
+    }
+  },
   methods: {
     navigateToHome(){
       this.$router.push('/')
